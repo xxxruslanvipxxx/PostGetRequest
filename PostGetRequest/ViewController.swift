8815f6d714970f10efd2154c1071d575b5398355
xxxruslanvipxxx <rysfan00@gmail.com>
2024-01-28 18:48:13 +0300

    Initial Commit

---
+++ PostGetRequest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostGetRequest
+//
+//  Created by Руслан Забиран on 28.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
