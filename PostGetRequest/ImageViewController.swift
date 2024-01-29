e0cca00f26019e404aacf947e78b2ac605cf06c5
xxxruslanvipxxx <rysfan00@gmail.com>
2024-01-29 20:24:32 +0300

    basic functionality implemented

---
+++ PostGetRequest/ImageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ImageViewController.swift
+//  PostGetRequest
+//
+//  Created by Руслан Забиран on 28.01.24.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
