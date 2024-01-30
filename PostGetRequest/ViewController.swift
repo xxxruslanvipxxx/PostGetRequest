2de33213bf37e314d933b7f13ce4525f70ca6232
xxxruslanvipxxx <rysfan00@gmail.com>
2024-01-30 22:37:41 +0300

    get request completed

--- PostGetRequest/ViewController.swift
+++ PostGetRequest/ViewController.swift
@@ -13,6 +13,23 @@ class ViewController: UIViewController {
 
     @IBAction func getRequest(_ sender: Any) {
         
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        let session = URLSession.shared
+        
+        session.dataTask(with: url) { (data, response, error) in
+            guard
+                let response = response,
+                    let data = data
+            else {return}
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }.resume()
+        
     }
     
     @IBAction func postRequest(_ sender: Any) {
