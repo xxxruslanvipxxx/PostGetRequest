0a32b9776a5b15582c33e23b4094c9d45821331a
xxxruslanvipxxx <rysfan00@gmail.com>
2024-01-31 22:21:34 +0300

    add postRequset func

--- PostGetRequest/ViewController.swift
+++ PostGetRequest/ViewController.swift
@@ -12,7 +12,6 @@ let segueString = "imageVIewControllerSegue"
 class ViewController: UIViewController {
 
     @IBAction func getRequest(_ sender: Any) {
-        
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
         
         let session = URLSession.shared
@@ -22,6 +21,7 @@ class ViewController: UIViewController {
                 let response = response,
                     let data = data
             else {return}
+            print(response)
             do {
                 let json = try JSONSerialization.jsonObject(with: data, options: [])
                 print(json)
@@ -33,7 +33,27 @@ class ViewController: UIViewController {
     }
     
     @IBAction func postRequest(_ sender: Any) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        let userData = ["Name": "Jeffrey", "Surname": "Epstein"]
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: userData) else {return}
         
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = httpBody
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { data, response, error in
+            guard let data = data, let response = response else {return}
+            print(response)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }.resume()
     }
     
     override func viewDidLoad() {
