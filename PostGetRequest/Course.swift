8bb3991930a70d06b756e2583ebf8d0ddfd0629a
xxxruslanvipxxx <rysfan00@gmail.com>
2024-02-01 21:37:34 +0300

    Properties changed to optional

--- PostGetRequest/Course.swift
+++ PostGetRequest/Course.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 struct Course: Decodable {
-    let id: Int
-    let name: String
-    let link: String
-    let imageUrl: String
-    let number_of_lessons: Int
-    let number_of_tests: Int
+    let id: Int?
+    let name: String?
+    let link: String?
+    let imageUrl: String?
+    let number_of_lessons: Int?
+    let number_of_tests: Int?
 }
