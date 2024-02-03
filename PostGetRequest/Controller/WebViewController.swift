09d5372def60b4384a69ba754d49505be18d7353
xxxruslanvipxxx <rysfan00@gmail.com>
2024-02-03 18:10:24 +0300

    refactor and add animate progressView

--- PostGetRequest/Controller/WebViewController.swift
+++ PostGetRequest/Controller/WebViewController.swift
@@ -14,27 +14,53 @@ class WebViewController: UIViewController {
     @IBOutlet weak var progressView: UIProgressView!
     
     var selectedCourse: String?
-    var courseURL = " "
+    var courseURL = ""
+    var estimatedProgressObserver: NSKeyValueObservation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = selectedCourse
-        setupWebView()
+        setupWebView(with: courseURL)
+        setupEstimatedProgressObserver()
     }
 
     
-    func setupWebView() {
-        guard let url = URL(string: courseURL) else {return}
+    func setupWebView(with urlString: String) {
+        guard let url = URL(string: urlString) else {return}
         let request = URLRequest(url: url)
         
         webView.load(request)
         webView.allowsBackForwardNavigationGestures = true
         webView.navigationDelegate = self
-        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+    }
+    
+    func setupEstimatedProgressObserver() {
+        estimatedProgressObserver = webView.observe(\.estimatedProgress, options: [.new]) { [weak self] webView, _ in
+            self?.progressView.setProgress(Float(webView.estimatedProgress), animated: true)
+        }
     }
 }
 
 extension WebViewController: WKNavigationDelegate {
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        if progressView.isHidden {
+            progressView.isHidden = false
+        }
+        
+        UIView.animate(withDuration: 0.33) {
+            self.progressView.alpha = 1
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        UIView.animate(withDuration: 0.33) {
+            self.progressView.alpha = 0
+        } completion: { isFinished in
+            self.progressView.isHidden = isFinished
+        }
+
+    }
     
 }
