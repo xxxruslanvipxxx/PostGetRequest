3e79b4883dac321b5c67c8983f65d847f9d1e855
xxxruslanvipxxx <rysfan00@gmail.com>
2024-02-06 20:54:33 +0300

    uploadImage func and new image in Assets

--- PostGetRequest/Support/NetworkManager.swift
+++ PostGetRequest/Support/NetworkManager.swift
@@ -6,7 +6,7 @@
 //
 
 //import UIKit
-import Foundation
+import UIKit
 
 class NetworkManager {
     
@@ -90,6 +90,34 @@ class NetworkManager {
             print(error.localizedDescription)
         }
         return nil
+    }
+    
+    func uploadImage(url: String) {
+        
+        let image = UIImage(named: "default")!
+        let httpHeaders = ["Autorization": "Client-ID bc172cceefa4123"]
+        guard let imageProperties = ImageProperties(withImage: image, forKey: "image") else { return }
+        
+        guard let url = URL(string: url) else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.allHTTPHeaderFields = httpHeaders
+        request.httpBody = imageProperties.data
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let response = response {
+                print(response)
+            }
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }.resume()
+    
     }
     
 }
