91a5b195ea8e4116f99a26cf427f8da90b3ad62a
xxxruslanvipxxx <rysfan00@gmail.com>
2024-02-25 00:41:36 +0300

    Setup facebook login button

--- PostGetRequest/Controller/LoginViewController.swift
+++ PostGetRequest/Controller/LoginViewController.swift
@@ -6,12 +6,47 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
 class LoginViewController: UIViewController {
-
+    
+    lazy var fbLoginButton: UIButton = {
+        let fbLoginButton = FBLoginButton()
+        fbLoginButton.frame = CGRect(x: 32, y: 320, width: view.frame.width - 64, height: 28)
+        fbLoginButton.delegate = self
+        
+        return fbLoginButton
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setupViews()
+        
+        if let token = AccessToken.current, !token.isExpired {
+            print(token.userID)
+            print(AccessToken.isCurrentAccessTokenActive)
+            print(token.expirationDate)
+        }
+    }
+    
+    private func setupViews() {
+        view.addSubview(fbLoginButton)
     }
 
+}
+
+extension LoginViewController: LoginButtonDelegate {
+    
+    func loginButton(_ loginButton: FBSDKLoginKit.FBLoginButton, didCompleteWith result: FBSDKLoginKit.LoginManagerLoginResult?, error: Error?) {
+        if let error = error {
+            print(error.localizedDescription)
+        }
+        print("Successfully logged in with facebook...")
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginKit.FBLoginButton) {
+        print("Logged out")
+    }
+    
 }
