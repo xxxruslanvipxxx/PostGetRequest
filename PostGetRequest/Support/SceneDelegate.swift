04ca20bb972a054cebda2304606cb0abeb9c31ca
xxxruslanvipxxx <rysfan00@gmail.com>
2024-02-24 16:34:18 +0300

    delete rootVC const

--- PostGetRequest/Support/SceneDelegate.swift
+++ PostGetRequest/Support/SceneDelegate.swift
@@ -13,9 +13,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let rootVC = window?.rootViewController
+        guard let _ = (scene as? UIWindowScene) else { return }
         
     }
 
