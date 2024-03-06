074aca6cf09c83d7128ee7fc892d53204a424c96
xxxruslanvipxxx <rysfan00@gmail.com>
2024-03-06 22:05:01 +0300

    Setup views and base methods

--- PostGetRequest/Controller/SignInVC.swift
+++ PostGetRequest/Controller/SignInVC.swift
@@ -9,21 +9,125 @@ import UIKit
 
 class SignInVC: UIViewController {
 
+    lazy var continueButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Continue", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.layer.cornerRadius = 4
+        button.alpha = 0.5
+        button.addTarget(self, action: #selector(handleContinueButton), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    lazy var signInLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Sign In"
+        
+        return label
+    }()
+    
+    lazy var emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = .white
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "email"
+        textField.textColor = .black
+        textField.clearButtonMode = .whileEditing
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+        
+        return textField
+    }()
+    
+    lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = .white
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "password"
+        textField.textColor = .black
+        textField.clearButtonMode = .whileEditing
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+        
+        return textField
+    }()
+    
+    lazy var signUpButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Sign Up", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(handleSignUpButton), for: .touchUpInside)
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.navigationController?.navigationBar.tintColor = .black
+        setupView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillAppear), name: UIResponder.keyboardWillShowNotification, object: nil)
     }
-    */
+    
+    private func setupView() {
+        view.addSubview(signInLabel)
+        signInLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100).isActive = true
+        signInLabel.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.5).isActive = true
+        signInLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        
+        view.addSubview(emailTextField)
+        emailTextField.topAnchor.constraint(equalTo: signInLabel.bottomAnchor, constant: 20).isActive = true
+        emailTextField.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.8).isActive = true
+        emailTextField.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        
+        view.addSubview(passwordTextField)
+        passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 20).isActive = true
+        passwordTextField.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.8).isActive = true
+        passwordTextField.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        
+        view.addSubview(signUpButton)
+        signUpButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 20).isActive = true
+        signUpButton.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.4).isActive = true
+        signUpButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        
+        view.addSubview(continueButton)
+        continueButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -100).isActive = true
+        continueButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        continueButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        continueButton.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.5).isActive = true
+    }
+    
+    @objc private func keyboardWillAppear() {  // FIX THIS MESS
+        continueButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -100).isActive = false
+        continueButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -340).isActive = true
+        continueButton.layoutIfNeeded()
+    }
+    
+    @objc private func textFieldChanged() {
+        
+    }
+    
+    @objc private func handleSignUpButton() {
+        performSegue(withIdentifier: "signUpSegue", sender: self)
+    }
+    
+    @objc private func handleContinueButton() {
+        
+    }
 
 }
